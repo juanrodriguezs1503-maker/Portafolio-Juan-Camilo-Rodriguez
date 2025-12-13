4307d9ec204a9d4c9ee08f31708f9ff8872bed7c
juanrodriguezs1503 <juan.rodriguezs1503@gmail.com>
2025-12-12 23:07:21 -0500

    Cambios principales y diseño princpipal

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const data = JSON.parse(localStorage.getItem("contactData"));
+  if (data) {
+    document.getElementById("savedMessage").textContent =
+      `Último mensaje enviado por ${data.name}`;
+  }
+});
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const contactData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+
+  localStorage.setItem("contactData", JSON.stringify(contactData));
+
+  document.getElementById("savedMessage").textContent =
+    "Mensaje enviado y guardado correctamente ✔";
+
+  this.reset();
+});
